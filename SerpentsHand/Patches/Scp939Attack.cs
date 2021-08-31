8e8f8d4940094e7e0058b3a5fe8c41bb3b41ec9b
Steven Webster <steven@is-just-a.dev>
2021-08-30 22:35:46 -0400

    I knew they would

--- SerpentsHand/Patches/Scp939Attack.cs
+++ SerpentsHand/Patches/Scp939Attack.cs
@@ -4,29 +4,29 @@ using UnityEngine;
 
 namespace SerpentsHand.Patches
 {
-	//[HarmonyPatch(typeof(PlayableScps.Scp939), nameof(PlayableScps.Scp939.ServerAttack))]
-	//class Scp939Attack
-	//{
-	//	public static void Postfix(PlayableScps.Scp939 __instance, Mirror.NetworkConnection conn, PlayableScps.Messages.Scp939AttackMessage msg)
-	//	{
-	//		Player player = Player.Get(msg.Victim);
-	//		if (player != null && EventHandlers.shPlayers.Contains(player.Id) && !SerpentsHand.instance.Config.FriendlyFire)
-	//		{
-	//			player.ReferenceHub.playerEffectsController.DisableEffect<CustomPlayerEffects.Amnesia>();
-	//		}
-	//	}
-	//}
-
-	[HarmonyPatch(typeof(Scp939PlayerScript), nameof(Scp939PlayerScript.UserCode_CmdShoot))]
+	[HarmonyPatch(typeof(PlayableScps.Scp939), nameof(PlayableScps.Scp939.ServerAttack))]
 	class Scp939Attack
 	{
-		public static void Postfix(Scp939PlayerScript __instance, GameObject target)
+		public static void Postfix(PlayableScps.Scp939 __instance, Mirror.NetworkConnection conn, PlayableScps.Messages.Scp939AttackMessage msg)
 		{
-			Player player = Player.Get(target);
+			Player player = Player.Get(msg.Victim);
 			if (player != null && EventHandlers.shPlayers.Contains(player.Id) && !SerpentsHand.instance.Config.FriendlyFire)
 			{
 				player.ReferenceHub.playerEffectsController.DisableEffect<CustomPlayerEffects.Amnesia>();
 			}
 		}
 	}
+
+	//[HarmonyPatch(typeof(Scp939PlayerScript), nameof(Scp939PlayerScript.UserCode_CmdShoot))]
+	//class Scp939Attack
+	//{
+	//	public static void Postfix(Scp939PlayerScript __instance, GameObject target)
+	//	{
+	//		Player player = Player.Get(target);
+	//		if (player != null && EventHandlers.shPlayers.Contains(player.Id) && !SerpentsHand.instance.Config.FriendlyFire)
+	//		{
+	//			player.ReferenceHub.playerEffectsController.DisableEffect<CustomPlayerEffects.Amnesia>();
+	//		}
+	//	}
+	//}
 }
